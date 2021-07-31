e92e631ac0d510f9fca8c323d85ff85f809378fe
bwon783 <bill.bw82262@hotmail.com>
2021-08-01 11:56:38 +1200

    Removed unnecessary imports

--- my-app/src/components/AddTaskTab.js
+++ my-app/src/components/AddTaskTab.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import Input from "./Input"
-import { Button } from 'semantic-ui-react'
-import App from '../App'
 import TaskInput from "./TaskInput"
 import SimpleMenu from "./SimpleMenu"
 import Countdown from "./Countdown"
