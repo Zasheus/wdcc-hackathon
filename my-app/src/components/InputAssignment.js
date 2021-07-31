86ab2d4bbee43e1ae5058220b3da8378501d426d
bwon783 <bill.bw82262@hotmail.com>
2021-07-31 15:17:09 +1200

    Added the Assignment input fields
    Need to make it return a value for backend.

--- my-app/src/components/InputAssignment.js
+++ my-app/src/components/InputAssignment.js
@@ -1,5 +1,22 @@
 const InputAssignment = () => {
     return (
+        <div>
+            <div className = "input-details">
+                <label>Assignment Name:</label>
+                <input type = "text" placeholder="Add Assignment"/>
+            </div>
+            <div className = "input-details">
+                <label>Due Date:</label>
+                <input type = "text" placeholder="Add Due Date"/>
+            </div>
+            <div className = "input-details">
+                <label>Time Needed:</label>
+                <input type = "text" placeholder="Add Assignment"/>
+            </div>
+
+        </div>
+        
+
         
     )
 }
