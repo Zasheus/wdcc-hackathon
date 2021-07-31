938face53cf5589f3eb3d1d1c0138510bee9cd4f
bwon783 <bill.bw82262@hotmail.com>
2021-07-31 15:30:45 +1200

    Tidying up the input assignment details

--- my-app/src/components/InputAssignment.js
+++ my-app/src/components/InputAssignment.js
@@ -3,7 +3,7 @@ const InputAssignment = () => {
         <div>
             <div className = "input-details">
                 <label>Assignment Name:</label>
-                <input type = "text" placeholder="Add Assignment"/>
+                <input type = "text" placeholder="Add Assignment Name"/>
             </div>
             <div className = "input-details">
                 <label>Due Date:</label>
@@ -11,8 +11,10 @@ const InputAssignment = () => {
             </div>
             <div className = "input-details">
                 <label>Time Needed:</label>
-                <input type = "text" placeholder="Add Assignment"/>
+                <input type = "text" placeholder="Add Time Required to finish assignment"/>
             </div>
+
+            <input type='submit' value='Add Assignment to Calender' className='submit-button'/>
 
         </div>
         
